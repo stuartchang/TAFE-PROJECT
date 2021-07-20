a617672ee0e69d264c5c725ce7158fa6db6f06f3
stuartchang <87692733+stuartchang@users.noreply.github.com>
2021-07-20 21:01:56 +0800

    Add files via upload

---
+++ TestNumberSwitchV3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Stuart Chang
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            bool keyBoardInput = false;
+            {
+                Console.Write("What is the number");
+                keyBoardInput = int.TryParse(Console.ReadLine(), out number);
+            }
+            if (keyBoardInput == true)
+            {
+                int range = (number / 10) * 10;
+                switch (range)
+                {
+                    case 0:
+                    case 1:
+                    case 3:
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                    case 8:
+                    case 9:
+                        Console.WriteLine("Less than 10");
+                        break;
+                    default:
+                        Console.WriteLine("More than 10");
+                        break;
+                }
+
+            }
+            else
+                Console.WriteLine("Invalid");
+            Console.ReadLine();
+
+        }
+    }
+}
