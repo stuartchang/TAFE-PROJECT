2a4e5955762e33ce21cb445126ec1e133b299f22
stuartchang <87692733+stuartchang@users.noreply.github.com>
2021-07-20 21:01:03 +0800

    Add files via upload

---
+++ TestNumberIf/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Stuart Chang
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            bool keyBoardInput = false;
+            {
+                Console.Write("What is the number");
+                keyBoardInput = int.TryParse(Console.ReadLine(), out number);
+            }
+            if (keyBoardInput == true)
+            {
+                if (number > 10)
+                    Console.WriteLine("More than 10");
+                else if (number < 10)
+                    Console.WriteLine("Less than 10");
+            }
+            else
+                Console.WriteLine("Invalid");
+            Console.ReadLine();
+            
+        }
+    }
+}
